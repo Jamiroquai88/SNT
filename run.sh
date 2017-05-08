b4775423093847d957d5ac86fc535cb79477b888
Profant Jan <xprofa00@merlin.fit.vutbr.cz>
2017-05-08 20:02:29 +0200

    FIX: run.sh

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ dataset=$1
 
 for f in $(find $CDIR/datasets/$dataset -name *.tim)
 do
-	$CDIR/main.py -i $f > $(echo $f | sed -e 's@\.tim@\.log@g') 2>&1 &
+	echo "$CDIR/main.py -i $f > $(echo $f | sed -e 's@\.tim@\.log@g') 2>&1"
 done
