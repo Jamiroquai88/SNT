ccd1f7189f6d9f9e6dc645659365ced1351bba52
Jamiroquai88 <Jamiroquai88@github.com>
2017-05-08 11:40:24 +0200

    ADD: run script.

---
+++ run.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+CDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+dataset=$1
+
+for f in $(find $CDIR/datasets/$dataset -name *.tim)
+do
+	$CDIR/main.py -i $f > $(echo $f | sed -e 's@\.tim@\.log@g') 2>&1 &
+done
