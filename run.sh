09f3b0a97f8e2d41e3691e295e1101bed40849c4
Profant Jan <xprofa00@merlin.fit.vutbr.cz>
2017-05-09 02:03:39 +0200

    FIX: run.sh

--- run.sh
+++ run.sh
@@ -2,8 +2,12 @@
 
 CDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 dataset=$1
+ITERS=100
 
-for f in $(find $CDIR/datasets/$dataset -name *.tim)
+for i in $(seq 1 100)
 do
-	echo "$CDIR/main.py -i $f > $(echo $f | sed -e 's@\.tim@\.log@g') 2>&1"
+    for f in $(find $CDIR/datasets/$dataset -name *.tim)
+    do
+        echo "$CDIR/main.py -i $f -o $(echo $f | sed -e "s@\.tim@__$i\.sln@g") > $(echo $f | sed -e "s@\.tim@__$i\.log@g") 2>&1"
+    done
 done
